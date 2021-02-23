e1a35ab25e0bbb91ee8a511e185913a9c11c0784
KimHung <michaeltkhchan@gmail.com>
2021-02-23 00:02:07 +0000

    Changed to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Containers/Home/Home";
